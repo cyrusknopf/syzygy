64dcc69cf94894e2d214239370056759459c530d
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2024-04-21 15:46:55 +0100

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -4,48 +4,39 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-23.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in with pkgs; rec {
-        devShell = mkShell {
+        devShell = mkShell rec {
           buildInputs = [
             rustc
             cargo
-            openssl
-            pkg-config
+
+
+
+
+
+
             libxkbcommon
             libGL
 
-            # Wayland
+            # WINIT_UNIX_BACKEND=wayland
             wayland
 
-            # X11
+            # WINIT_UNIX_BACKEND=x11
             xorg.libXcursor
             xorg.libXrandr
             xorg.libXi
             xorg.libX11
           ];
-
-          shellHook = ''
-            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(echo ${lib.makeLibraryPath [
-              rustc
-              cargo
-              openssl
-              pkg-config
-              libxkbcommon
-              libGL
-              wayland
-              xorg.libXcursor
-              xorg.libXrandr
-              xorg.libXi
-              xorg.libX11
-            ]})"
-            export WINIT_UNIX_BACKEND=x11  # Force X11 backend
-            # Uncomment the following line to use Wayland instead of X11
-            # export WINIT_UNIX_BACKEND=wayland
-          '';
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
         };
       });
 }
